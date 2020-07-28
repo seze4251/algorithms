8f5a09a366e37fc8ed2b52699613f05634ce14ad
Seth Zegelstein <12955695+seze4251@users.noreply.github.com>
2020-07-28 00:05:49 -0600

    updating stack class

--- stack/stack.h
+++ stack/stack.h
@@ -3,6 +3,14 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+
+// TODO: Look at std::stack and fix user facing API
+//       Add missing methods, and adjust implementation to match theirs
+//       Think about how to implement move semantics
+// TODO: Look into a more C++ way to do realloc
+// TODO: 100% UT coverage with google test, requires more of a build system than command line g++ lol
+
+
 template <typename T>
 class Stack {
 public:
@@ -14,12 +22,16 @@ public:
 		}
 
 		// Cannot use make_unique b/c C++ doesn't support realloc
-		// If you want to use realloc, you must use malloc
+		// If you want to use realloc, you must use malloc for defined behavior
 		stack_.reset(reinterpret_cast<T*>(malloc(capacity_*sizeof(T))));
 	}
 
 	void push(T data) {
 		if (idx_ == capacity_) {
+			if (!auto_resize_) {
+				throw std::runtime_error("Stack is full and cannot alloc b/c auto_resize is set to false");
+			}
+
 			capacity_ = 2 * capacity_;
 			stack_.reset(reinterpret_cast<T*>(realloc(stack_.release(), capacity_*sizeof(T))));
 
@@ -32,13 +44,16 @@ public:
 		idx_++;
 	}
 
-
+	// Returns a pointer to the top element in the stack
 	T* peek() {
-		return &stack_[idx_-1];
+		if (idx_ == 0) {
+			return nullptr;
+		}
 
+		return &stack_[idx_-1];
 	}
 
-
+	// Returns the top element in the stack, and removes it from the stack
 	T pop () {
 		if (idx_ == 0) {
 			throw std::runtime_error("Cannot get element b/c not elements are on stack");
@@ -49,6 +64,6 @@ public:
 private:
 	std::unique_ptr<T[]> stack_; ///< Structure that holds the stack
 	size_t idx_ = 0; ///< Index to insert into
-	size_t capacity_;
-	bool auto_resize_;
+	size_t capacity_; ///< Maximum # of T elements allowed on vector
+	bool auto_resize_; ///< Allow users to get an exception instead of auto-resizing
 };
--- stack/test_stack.cpp
+++ stack/test_stack.cpp
@@ -3,6 +3,7 @@
 
 
 int main() {
+	std::cout << "Starting Stack Tests" << std::endl;
 	size_t capacity = 1;
 	Stack<int> stack(capacity);
 	stack.push(5);
@@ -10,5 +11,7 @@ int main() {
 	assert(*stack.peek() == 19);
 	assert(stack.pop() == 19);
 	assert(stack.pop() == 5);
+	// Yes I know these tests are horrible, but I am tired and will fix later
+	std::cout << "Successfully ran all of Stack Tests" << std::endl;
 	return 0;
 }
